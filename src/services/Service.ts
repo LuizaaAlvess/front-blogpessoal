5d0c1ff9c93800043064fb26d30ae4abc99c5e26
LuizaaAlvess <luiza.andreza@icloud.com>
2026-02-26 22:27:25 -0300

    arr

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,35 +1,34 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://backend-nest-yxh4.onrender.com'
-                baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL,
 })
 
-export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
+export const cadastrarUsuario = async (url: string, dados: any, setDados: (d: any) => void) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
 }
 
-export const login = async (url: string, dados: Object, setDados: Function) => {
+export const login = async (url: string, dados: any, setDados: (d: any) => void) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
 }
 
-export const buscar = async (url: string, setDados: Function, header: Object) => {
+export const buscar = async (url: string, setDados: (d: any) => void, header?: any) => {
     const resposta = await api.get(url, header)
     setDados(resposta.data)
 }
 
-export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+export const cadastrar = async (url: string, dados: any, setDados: (d: any) => void, header?: any) => {
     const resposta = await api.post(url, dados, header)
     setDados(resposta.data)
 }
 
-export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+export const atualizar = async (url: string, dados: any, setDados: (d: any) => void, header?: any) => {
     const resposta = await api.put(url, dados, header)
     setDados(resposta.data)
 }
 
-export const deletar = async (url: string, header: Object) => {
+export const deletar = async (url: string, header?: any) => {
     await api.delete(url, header)
 }
